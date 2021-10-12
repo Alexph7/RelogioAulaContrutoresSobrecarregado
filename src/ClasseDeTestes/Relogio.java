2f9783d3ac3b40a4f3352eb5c4fa541972f9b361
alexph7 <alexph7.56@gmail.com>
2021-10-12 14:16:53 -0300

    Get e Set Configurados

--- src/ClasseDeTestes/Relogio.java
+++ src/ClasseDeTestes/Relogio.java
@@ -2,27 +2,34 @@ package ClasseDeTestes;
 
 public class Relogio {
 
-    private int hora;
-    private int minutos;
-    private int segundos;
+    private int hora; //Entre 0-24
+    private int minutos; //Entre 0-59
+    private int segundos; //Entre 0-59
 
+    //Construtor Sem Parametros
+    //Inicializa para 0 cada Variavel de instancia
     public Relogio() {
-        this(0, 0, 0);
+        this(0, 0, 0); // Invoca o Construtor Com 3 Argumentos
     }
 
+    //Construtor Com 1 Parametro
+    //Hora Fornecida e Minutos e segundos inicializados para 0
     public Relogio(int hora) {
-        this(hora, 0, 0);
+        this(hora, 0, 0); // Invoca o Construtor Com 3 Argumentos
     }
 
+    // Construtor Com 2 Parametros
+    //Hora e Minutos Fornecidos e segundos Inicializado para 0
     public Relogio(int hora, int minutos) {
-        this(hora, minutos, 0);
+        this(hora, minutos, 0); // Invoca o Construtor Com 3 Argumentos
     }
 
+    //Construtor Com 3 Parametros
     public Relogio(int hora, int minutos, int segundos) {
-        if (hora < 0 || hora >=24) {
+        if (hora < 0 || hora >= 24) {
             throw new IllegalArgumentException("Hora Deve Ser Entre 0 - 23");
         }
-        if (minutos < 0 || minutos >=60) {
+        if (minutos < 0 || minutos >= 60) {
             throw new IllegalArgumentException("Minutos Deve Ser Entre 0 - 59");
         }
         if (segundos < 0 || segundos >= 60) {
@@ -33,15 +40,20 @@ public class Relogio {
         this.segundos = segundos;
     }
 
+    //Contrutor com objeto Relogio Fornecido
     public Relogio(Relogio r) {
-        this(r.getHora(), r.getMinutos(), r.getSegundos());
+        this(r.getHora(), r.getMinutos(), r.getSegundos()); // Invoca o Construtor Com 3 Argumentos
     }
 
     public int getHora() {
         return hora;
     }
 
+    //Valida e Configura a Hora
     public void setHora(int hora) {
+        if (hora < 0 || hora >= 24) {
+            throw new IllegalArgumentException("Hora Deve Ser Entre 0 - 23");
+        }
         this.hora = hora;
     }
 
@@ -49,7 +61,11 @@ public class Relogio {
         return minutos;
     }
 
+    //Valida e configura os Minutos
     public void setMinutos(int minutos) {
+        if (minutos < 0 || minutos >= 60) {
+            throw new IllegalArgumentException("Minutos Deve Ser Entre 0 - 59");
+        }
         this.minutos = minutos;
     }
 
@@ -57,7 +73,11 @@ public class Relogio {
         return segundos;
     }
 
+    //Valida e configura os Segundos
     public void setSegundos(int segundos) {
+        if (segundos < 0 || segundos >= 60) {
+            throw new IllegalArgumentException("Segundos Deve Ser Entre 0 - 59");
+        }
         this.segundos = segundos;
     }
 
