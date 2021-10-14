69e34b636cfd846f45b5ab1f37be49fc475fc0a8
Alexph7 <alexph7.56@gmail.com>
2021-10-14 15:00:02 -0300

    arquivos complementares

--- src/ClasseDeTestes/Testes.java
+++ src/ClasseDeTestes/Testes.java
@@ -4,7 +4,8 @@ public class Testes {
     
     public static void main(String[] args) {
         Relogio re = new Relogio(2);
-        
-        System.out.println(re.toString());
+
+        System.out.println(re.horaUniversal());
+
     }
 }
